23bfa803bdf568c771ea3bfda1b2b273c98f3404
Richard T <richard.tambong04@gmail.com>
2023-09-01 00:31:17 +0800

    update tracks

--- script.js
+++ script.js
@@ -7,32 +7,69 @@ const progress = document.getElementById("progress");
 const currentTimeText = document.getElementById("current-time");
 const totalTimeText = document.getElementById("total-time");
 const previousButton = document.querySelector(".previous-button");
+const shuffleButton = document.querySelector(".shuffle-button");
 
 const songs = [
   {
-    title: "Bawat Piyesa - Munimuni",
-    artist: "Track 01",
+    title: "Bawat Piyesa",
+    artist: "Track 01 - Munimuni",
     source: "./songs/01_Munimuni_Bawat_Piyesa.mp3",
     cover: "./media/thumbnail.png",
   },
   {
-    title: "Talinghaga - Munimuni",
-    artist: "Track 02",
-    source: "./songs/02_Talinghaga_Munimuni.mp3",
+    title: "Sayo",
+    artist: "Track 02 - Munimuni",
+    source: "./songs/02_Munimuni_Sayo.mp3",
     cover: "./media/thumbnail.png",
   },
   {
-    title: "Eba - Kiyo",
-    artist: "Track 03",
-    source: "./songs/03_Kiyo_Eba.mp3",
+    title: "Talinghaga",
+    artist: "Track 03 - Munimuni",
+    source: "./songs/03_Munimuni_Talinghaga.mp3",
     cover: "./media/thumbnail.png",
   },
   {
-    title: "Paraluman - Adie",
-    artist: "Track 04",
-    source: "./songs/04_Paraluman_Adie.mp3",
+    title: "Musika",
+    artist: "Track 04 - Dionela",
+    source: "./songs/04_Dionela_Musika.mp3",
     cover: "./media/thumbnail.png",
   },
+  {
+    title: "Paraluman",
+    artist: "Track 05 - Adie",
+    source: "./songs/05_Paraluman_Adie.mp3",
+    cover: "./media/thumbnail.png",
+  },
+  {
+    title: "Eba",
+    artist: "Track 06 - Kiyo",
+    source: "./songs/06_Kiyo_Eba.mp3",
+    cover: "./media/thumbnail.png",
+  },
+  {
+    title: "Balang Araw",
+    artist: "Track 07 - I Belong To The Zoo",
+    source: "./songs/07_I_Belong_To_The_Zoo_Balang_Araw.mp3",
+    cover: "./media/thumbnail.png",
+  },
+  {
+    title: "Sana",
+    artist: "Track 08 - I Belong To The Zoo",
+    source: "./songs/08_I_Belong_To_The_Zoo_Sana.mp3",
+    cover: "./media/thumbnail.png",
+  },
+  {
+    title: "Kundiman",
+    artist: "Track 09 - Silent Sanctuary",
+    source: "./songs/09_Silent_Sanctuary_Kundiman.mp3",
+    cover: "./media/thumbnail.png",
+  },
+  {
+    title: "Rebound",
+    artist: "Track 10 - Silent Sanctuary",
+    source: "./songs/10_Silent_Sanctuary_Rebound.mp3",
+    cover: "./media/thumbnail.png",
+  }
 ];
 
 let currentSongIndex = 0;
